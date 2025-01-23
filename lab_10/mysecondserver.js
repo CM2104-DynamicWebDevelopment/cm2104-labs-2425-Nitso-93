2fffb7fbcd3f427449f391a1d86d80185cf5cc14
Ostin Mathias <o.mathias@rgu.ac.uk>
2025-01-23 11:18:00 +0000

    added spacing

--- lab_10/mysecondserver.js
+++ lab_10/mysecondserver.js
@@ -6,6 +6,6 @@ http.createServer(function (req, res) {
 
  res.writeHead(200, {'Content-Type': 'text/html'});
  res.write("The date and time are currently: "+currentdate.myDateTime());
- res.end('Hello World!');
+ res.end('\nHello World!');
  
 }).listen(8080);
