62721068ee9fb3f31919d312483506f39325c0d9
Ostin Mathias <o.mathias@rgu.ac.uk>
2025-01-30 12:00:08 +0000

    Added query route

--- lab_11/server.js
+++ lab_11/server.js
@@ -2,19 +2,30 @@ var express = require('express');
 var knockknock=require('knock-knock-jokes');
 var app = express();
 
+//Main Route
 app.get('/', function(req, res)
 {
     res.send("Hello World! by Express");
 });
 
+//Extra Route
 app.get('/test', function(req,res)
 {
     res.send("this is route 2");
 });
 
+// Knock Knock Joke Route
 app.get('/joke', function(req,res)
 {
     res.send(knockknock());
+});
+
+//Query Route
+app.get('/add', function(req,res)
+{
+    var x= req.query.x;
+    var y= req.query.y;
+    res.send("X + Y = "+(x+y));
 });
 
 app.listen(8080);
